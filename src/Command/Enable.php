1ad5cf636b6eb1f485af2e9e75b9850aff156e2d
fab2s <fabrice.de.stefanis@gmail.com>
2022-12-06 14:05:50 +0100

    Enable --model

--- src/Command/Enable.php
+++ src/Command/Enable.php
@@ -24,8 +24,9 @@ class Enable extends Command
      *
      * @var string
      */
-    protected $signature = 'searchable:enable 
+    protected $signature = 'searchable:enable
             {--root= : The place where to start looking for models, defaults to Laravel\'s app/Models}
+            {--model= : To narrow to a single App/Model/ClassName by FQN}
             {--index : To also index / re index}';
 
     /**
@@ -44,6 +45,7 @@ class Enable extends Command
      * @var bool
      */
     protected bool $index;
+    protected string $model;
 
     /**
      * Execute the console command.
@@ -53,9 +55,21 @@ class Enable extends Command
     public function handle()
     {
         $this->index        = (bool) $this->option('index');
-        $this->modelRootDir = $this->option('root');
-        if (empty($this->modelRootDir)) {
-            $this->modelRootDir = app_path('app/Models');
+        $this->modelRootDir = $this->option('root')  ??  app_path('Models');
+        $this->model        = $this->option('model') ??  '';
+
+        if ($this->model) {
+            $this->model = str_replace('/', '\\', $this->model);
+            if (!class_exists($this->model)) {
+                $this->error('Provided Model FQN not found: ' . $this->model);
+
+                return 1;
+            }
+
+            $this->handleModel($this->model);
+            $this->output->success('Done');
+
+            return 0;
         }
 
         if (!is_dir($this->modelRootDir)) {
@@ -67,52 +81,69 @@ class Enable extends Command
         $this->getModelFiles();
         $foundSome = false;
         foreach (get_declared_classes() as $fqn) {
-            if (in_array(Searchable::class, class_uses_recursive($fqn), true)) {
-                $this->output->info("Processing $fqn");
-                $foundSome = true;
-                /** @var Model&Searchable $instance */
-                $instance        = new $fqn;
-                $searchableField = $instance->getSearchableField();
-                $table           = $instance->getTable();
-                $connection      = $instance->getConnectionName();
-                $dbType          = $instance->getSearchableFieldDbType();
-                $dbSize          = $instance->getSearchableFieldDbSize();
-
-                if (!Schema::connection($connection)->hasColumn($table, $searchableField)) {
-                    $this->output->info("Adding $searchableField to $table on $connection");
-                    $after = null;
-                    if ($instance->usesTimestamps()) {
-                        $columns = Schema::connection($connection)->getColumnListing($table);
-                        $after   = $this->getFirstBeforeAtField($columns);
-                    }
-
-                    Schema::connection($connection)->table($table, function (Blueprint $table) use ($searchableField, $after, $dbType, $dbSize) {
-                        $this->output->info("Using spec $dbType $dbSize");
-                        if ($after) {
-                            $this->output->info("After $after");
-                            $table->$dbType($searchableField, $dbSize)->default('')->after($after);
-                        } else {
-                            $table->$dbType($searchableField, $dbSize)->default('');
-                        }
-                    });
-
-                    $this->output->info('Create full text index');
-                    DB::connection($connection)->statement("ALTER TABLE $table ADD FULLTEXT searchable($searchableField)");
-                }
-
-                if ($this->index) {
-                    $this->index($instance);
-                }
-            }
+            $this->handleModel($fqn);
         }
 
-        if ($foundSome) {
-            $this->output->success('Done');
-        } else {
+        if (!$foundSome) {
             $this->warn('Could not find any model using Searchable trait in ' . $this->modelRootDir);
+        } else {
+            $this->output->success('Done');
         }
 
         return 0;
+    }
+
+    public function handleModel(string $fqn): static
+    {
+        if (in_array(Searchable::class, class_uses_recursive($fqn), true)) {
+            $this->output->info("Processing $fqn");
+            $foundSome = true;
+            /** @var Model&Searchable $instance */
+            $instance = new $fqn;
+            $this->configureModel($instance);
+
+            if ($this->index) {
+                $this->index($instance);
+            }
+        }
+
+        return $this;
+    }
+
+    protected function configureModel(Model $model): static
+    {
+        /** @var Model&Searchable $model */
+        $searchableField = $model->getSearchableField();
+        $table           = $model->getTable();
+        $connection      = $model->getConnectionName();
+        $dbType          = $model->getSearchableFieldDbType();
+        $dbSize          = $model->getSearchableFieldDbSize();
+
+        if (!Schema::connection($connection)->hasColumn($table, $searchableField)) {
+            $this->output->info("Adding $searchableField to $table");
+            $after = null;
+            if ($model->usesTimestamps()) {
+                $columns = Schema::connection($connection)->getColumnListing($table);
+                $after   = $this->getFirstBeforeAtField($columns);
+            }
+
+            Schema::connection($connection)->table($table, function (Blueprint $table) use ($searchableField, $after, $dbType, $dbSize) {
+                $this->output->info("Using spec $dbType $dbSize");
+                if ($after) {
+                    $this->output->info("After $after");
+                    $table->$dbType($searchableField, $dbSize)->default('')->after($after);
+                } else {
+                    $table->$dbType($searchableField, $dbSize)->default('');
+                }
+            });
+
+            $this->output->info('Create full text index');
+            DB::connection($connection)->statement("ALTER TABLE $table ADD FULLTEXT searchable($searchableField)");
+        } else {
+            $this->output->info("Found $searchableField in $table");
+        }
+
+        return $this;
     }
 
     /**
@@ -122,6 +153,7 @@ class Enable extends Command
     {
         /** @var Searchable $instance */
         $searchableField = $instance->getSearchableField();
+        $this->output->info('Indexing: ' . $instance::class);
         $this->output->progressStart();
         foreach ($instance->lazy() as $record) {
             /* @var  Model&Searchable $record */
