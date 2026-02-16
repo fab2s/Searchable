ebb35458be73b113d994108115c2743c79b6d0fa
fab2s <fabrice.de.stefanis@gmail.com>
2026-02-16 21:49:26 +0100

    fix headers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
- * This file is part of fab2s/laravel-dt0.
- * (c) Fabrice de Stefanis / https://github.com/fab2s/laravel-dt0
+ * This file is part of fab2s/searchable.
+ * (c) Fabrice de Stefanis / https://github.com/fab2s/Searchable
  * This source file is licensed under the MIT license which you will
  * find in the LICENSE file or at https://opensource.org/licenses/MIT
  */
