e26d9b27bfeeaf8fc4ffcb94fa0530f78eadf9e3
fab2s <fabrice.de.stefanis@gmail.com>
2026-02-19 00:56:52 +0100

    fix for laravel 10

--- tests/ListenerTest.php
+++ tests/ListenerTest.php
@@ -15,10 +15,22 @@ use fab2s\Searchable\Listener\SearchableEnableAfterMigrate;
 use Illuminate\Database\Events\MigrationsEnded;
 use Illuminate\Support\Facades\Artisan;
 use Mockery;
+use ReflectionClass;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 class ListenerTest extends TestCase
 {
+    /**
+     * MigrationsEnded gained the $options parameter in Laravel 11.
+     */
+    private static function supportsOptions(): bool
+    {
+        return (new ReflectionClass(MigrationsEnded::class))
+            ->getConstructor()
+            ->getNumberOfParameters() > 1
+        ;
+    }
+
     public function test_runs_enable_after_up_migration(): void
     {
         $mock = Mockery::mock();
@@ -42,6 +54,10 @@ class ListenerTest extends TestCase
 
     public function test_skips_on_pretend(): void
     {
+        if (! self::supportsOptions()) {
+            $this->markTestSkipped('MigrationsEnded does not support options before Laravel 11');
+        }
+
         $mock = Mockery::mock();
         $mock->shouldReceive('call')->never();
         Artisan::swap($mock);
@@ -51,6 +67,10 @@ class ListenerTest extends TestCase
 
     public function test_runs_when_pretend_is_false(): void
     {
+        if (! self::supportsOptions()) {
+            $this->markTestSkipped('MigrationsEnded does not support options before Laravel 11');
+        }
+
         $mock = Mockery::mock();
         $mock->shouldReceive('call')
             ->once()
@@ -63,6 +83,10 @@ class ListenerTest extends TestCase
 
     public function test_runs_when_options_missing_pretend(): void
     {
+        if (! self::supportsOptions()) {
+            $this->markTestSkipped('MigrationsEnded does not support options before Laravel 11');
+        }
+
         $mock = Mockery::mock();
         $mock->shouldReceive('call')
             ->once()
